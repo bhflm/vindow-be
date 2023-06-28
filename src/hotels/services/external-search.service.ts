fbcd9d119fb0d12686e318b0a715553e490bfef3
germi <germanbonin@gmail.com>
2023-06-28 19:02:27 -0300

    Fix: update endpoint, return list of hotels as a result

--- src/hotels/services/external-search.service.ts
+++ src/hotels/services/external-search.service.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Injectable } from '@nestjs/common';
 import { serializeSearchResponse } from '../serializer/external-search.serializer';
-
+import util from 'util';
 @Injectable()
 export class ExternalSearchService {
   private readonly API_KEY = process.env.GOOGLE_PLACES_API_KEY;
@@ -11,14 +11,17 @@ export class ExternalSearchService {
   async searchHotels(hotelName: string, address = null): Promise<any> {
     const baseUrl = `${this.GOOGLE_PLACES_API_URL}/textsearch/json`;
 
-    const query = `query=${encodeURIComponent(hotelName + (address && ' ' + address))}`;
+    const qs = address ? hotelName + address : hotelName;
 
+    const query = `query=${encodeURIComponent(qs)}`;
+    console.log('query: ', query);
     const url = `${baseUrl}?${query}&key=${this.API_KEY}`;
 
     try {
       const { data } = await axios.get(url);
-      const [ externalHotelResponse ] = serializeSearchResponse(data);
-      return externalHotelResponse;
+      console.log('data: ', JSON.stringify(data));
+      const externalHotelsResponse = serializeSearchResponse(data);
+      return externalHotelsResponse;
     } catch (error) {
       throw new Error('Failed to fetch hotel information from the external API.');
     }
