4a0f290807efb49868ca5347fb9903bdbb4a691c
germi <germanbonin@gmail.com>
2023-07-04 01:54:24 -0300

    Update: stack.ts

--- infra/stack.ts
+++ infra/stack.ts
@@ -1,3 +1,4 @@
+import { Asset } from 'aws-cdk-lib/aws-s3-assets';
 import * as CDK from 'aws-cdk-lib';
 import * as EB from 'aws-cdk-lib/aws-elasticbeanstalk';
 import { Construct } from 'constructs';
@@ -5,39 +6,82 @@ import * as IAM from 'aws-cdk-lib/aws-iam';
 
 import { globalCfg } from '../config';
 
+const appName = globalCfg.APP_NAME;
+
 export class CDKNestStack extends CDK.Stack {
   constructor(scope: Construct, id: string, props?: CDK.StackProps) {
     super(scope, id, props);
+  
+  const webAppZipArchive = new Asset(this, `${appName}-deploy`, {
+    path: `${__dirname}/../../${appName}-deploy.zip`,
+  });
 
-    // const platform = this.node.tryGetContext('platform');
+  const app = new EB.CfnApplication(this, 'Application', {
+    applicationName: appName
+  });
 
-    const instanceProfileRole = new IAM.Role(this, 'InstanceProfileRole', {
-      roleName: 'InstanceProfileRoleEB',
-      assumedBy: new IAM.ServicePrincipal('ec2.amazonaws.com')
-    });
-    // Create the instance profile and associate it with the role
-    const instanceProfile = new IAM.CfnInstanceProfile(this, 'InstanceProfile', {
-      roles: [instanceProfileRole.roleName]
-    });
+  const appVersionProps = new EB.CfnApplicationVersion(this, 'AppVersion', {
+  applicationName: appName,
+  sourceBundle: {
+      s3Bucket: webAppZipArchive.s3BucketName,
+      s3Key: webAppZipArchive.s3ObjectKey,
+  },
+  description: 'cdk-eb-appdeploy',
+  }); // end appVersionProps
 
-    const options: EB.CfnEnvironment.OptionSettingProperty[] = [
-      {
-        namespace: 'aws:autoscaling:launchconfiguration',
-        optionName: 'IamInstanceProfile',
-        value: instanceProfile.ref,
-      },
-    ];
+  appVersionProps.addDependency(app);
 
-    const ebApp = new EB.CfnApplication(this, `${globalCfg.APP_NAME}-app`, {
-      applicationName: globalCfg.APP_NAME,
-    });
 
-    const ebEnv = new EB.CfnEnvironment(this, `${globalCfg.APP_NAME}-env`, {
-      environmentName: globalCfg.APP_STAGE_NAME,
-      applicationName: ebApp.ref,
-      optionSettings: options,
-      solutionStackName: '64bit Amazon Linux 2 v5.8.3 running Node.js 18',
-    });
-    
+  const ec2Role = new IAM.Role(this, `${appName}-aws-elasticbeanstalk-ec2-role`, {
+    assumedBy: new IAM.ServicePrincipal('ec2.amazonaws.com'),
+  });
+
+  const managedPolicy = IAM.ManagedPolicy.fromAwsManagedPolicyName('AWSElasticBeanstalkWebTier')
+  ec2Role.addManagedPolicy(managedPolicy);
+  const myProfileName = `${appName}-InstanceProfile`;
+
+  const instanceProfile = new IAM.CfnInstanceProfile(this, myProfileName, {
+    instanceProfileName: myProfileName,
+    roles: [
+      ec2Role.roleName
+    ]
+  });
+
+  const optionSettingProperties: EB.CfnEnvironment.OptionSettingProperty[] = [
+    {
+      namespace: 'aws:autoscaling:launchconfiguration',
+      optionName: 'IamInstanceProfile',
+      value: myProfileName,
+  },
+  {
+      namespace: 'aws:autoscaling:asg',
+      optionName: 'MinSize',
+      value: '1',
+  },
+  {
+      namespace: 'aws:autoscaling:asg',
+      optionName: 'MaxSize',
+      value: '1',
+  },
+  {
+      namespace: 'aws:ec2:instances',
+      optionName: 'InstanceTypes',
+      value: 't2.micro',
+  },
+  {
+    namespace: 'aws:elasticbeanstalk:application:environment',
+    optionName: 'HEALTH_CHECK_URL',
+    value: '/health',
+  }
+  ];
+
+  const elbEnv = new EB.CfnEnvironment(this, 'Environment', {
+    environmentName: `${appName}-NodeAppEnvironment`,
+    applicationName: app.applicationName,
+    solutionStackName: '64bit Amazon Linux 2 v5.8.3 running Node.js 18',
+    optionSettings: optionSettingProperties,
+    versionLabel: appVersionProps.ref,
+  });
+
   }
 }
